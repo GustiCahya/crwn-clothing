f596549c34d889c800c18ed6d0e3014dd98d974a
GustiCahya <gusticahyax@yahoo.com>
2020-04-28 21:34:18 +0800

    added redux, and implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -6,41 +6,40 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import Header from './components/header/header.component'
 import { Switch, Route } from 'react-router-dom';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import {connect} from 'react-redux'
+import {setCurrentUser} from './redux/user/user.actions'
 
 class App extends React.Component{
-  constructor(){
-    super()
-    this.state={
-      currentUser:null
-    }
-  }
+  
   unSubscribeFromAuth = null
-  componentDidMount(){
-    this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 
+  componentDidMount(){
+    const {setCurrentUser} = this.props
+
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth)
         userRef.onSnapshot((snapshot)=>{
-          this.setState({
-            currentUser: {
-              id: snapshot.id,
-              ...snapshot.data()
-            }
+          setCurrentUser({
+            id: snapshot.id,
+            ...snapshot.data()
           })
         })
       }else{
-        this.setState({currentUser: userAuth})
+        setCurrentUser(userAuth)
       }
 
     })   
   }
+
   componentWillUnmount(){
     this.unSubscribeFromAuth()
   }
+
   render(){
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header/>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
@@ -51,4 +50,10 @@ class App extends React.Component{
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => (
+  {
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+  }
+)
+
+export default connect(null, mapDispatchToProps)(App);
